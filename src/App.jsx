d827db3dde846e4c93856c57120655f4f76714aa
Elian Gerard <eliangerardxd@gmail.com>
2024-02-19 21:57:53 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { Board } from './homponents/Board'
+
+function App() {
+  return (
+    <>
+      <Board/>
+    </>
+  )
+}
+
+export default App
